1c5fbd6c62bd8bb9097e5f52dcf0d1429fc5a719
gracefullight <gracefullight.dev@gmail.com>
2024-10-01 12:02:32 +0900

    feat: ✨ commitlint scope

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,7 +1,45 @@
 import type { UserConfig } from "@commitlint/types";
 
-const config: UserConfig = {
-  extends: ["@commitlint/config-conventional"],
-};
+import { existsSync, readFileSync, readdirSync, statSync } from "node:fs";
+import { join, resolve } from "node:path";
+import { parse } from "yaml";
 
-export default config;
+/**
+ * ? pnpm-workspace.yaml 모노레포 디렉토리 반환
+ *
+ * @returns string[] 1뎁스 폴더명
+ */
+function getScopesFromPnpmWorkspace(): string[] {
+  const workspaceFilePath = resolve(process.cwd(), "pnpm-workspace.yaml");
+
+  // ? pnpm-workspace.yaml 파일 동기적으로 읽기
+  const workspaceFile = readFileSync(workspaceFilePath, "utf8");
+  const workspaceConfig = parse(workspaceFile) as { packages: string[] };
+
+  const scopes: string[] = [];
+  // ? packages 배열을 읽어서 폴더 경로에서 디렉토리명 추출
+  for (const packagePathPattern of workspaceConfig.packages) {
+    const basePath = packagePathPattern.split("/*")[0]; // apps/* -> apps
+    const fullPath = resolve(process.cwd(), basePath);
+
+    if (existsSync(fullPath)) {
+      const dirs = readdirSync(fullPath).filter((file) => {
+        const stat = statSync(join(fullPath, file));
+        return stat.isDirectory();
+      });
+
+      scopes.push(...dirs);
+    }
+  }
+
+  return scopes;
+}
+
+const allowedScopes = getScopesFromPnpmWorkspace();
+
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [2, "always", allowedScopes], // 동적으로 가져온 스코프를 적용
+  },
+} as UserConfig;
