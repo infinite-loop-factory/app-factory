f3e36254df21581b78abcd33081d974f8f189ec7
gracefullight <gracefullight.dev@gmail.com>
2024-10-14 22:59:04 +0900

    fix: 🐛 biome 관련 코멘트 수정
    ref: #2

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,15 +1,10 @@
 export default {
   "*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}": [
-    "biome check --files-ignore-unknown=true", // Check formatting and lint
-    "biome check --write --no-errors-on-unmatched", // Format, sort imports, lint, and apply safe fixes
-    "biome check --write --organize-imports-enabled=false --no-errors-on-unmatched", // format and apply safe fixes
-    "biome check --write --unsafe --no-errors-on-unmatched", // Format, sort imports, lints, apply safe/unsafe fixes
-    "biome format --write --no-errors-on-unmatched", // Format
-    "biome lint --write --no-errors-on-unmatched", // Lint and apply safe fixes
+    "biome check --fix --unsafe --no-errors-on-unmatched", // Format, sort imports, lints, apply safe/unsafe fixes
   ],
   // Alternatively you can pass every files and ignore unknown extensions
   "*": [
-    "biome check --no-errors-on-unmatched --files-ignore-unknown=true", // Check formatting and lint
+    "biome check --fix --no-errors-on-unmatched --files-ignore-unknown=true", // Check formatting and lint
   ],
   "package.json": "sort-package-json",
 };
