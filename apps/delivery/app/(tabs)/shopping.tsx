9363cc6f5ff808a56334f62facbde7ce6a32177e
chakyubeom <ckrbqja@gmail.com>
2024-10-23 23:01:18 +0900

    feat: ✨ setting layout

--- apps/delivery/app/(tabs)/shopping.tsx
+++ apps/delivery/app/(tabs)/shopping.tsx
@@ -1,10 +1,10 @@
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function HomeScreen() {
+export default function Shopping() {
   return (
     <SafeAreaView>
-      <Text>홈</Text>
+      <Text>장보기</Text>
     </SafeAreaView>
   );
 }
--- apps/delivery/constants/Colors.ts
+++ apps/delivery/constants/Colors.ts
@@ -3,8 +3,8 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = "#0a7ea4";
-const tintColorDark = "#fff";
+const tintColorLight = "#00C2B3";
+const tintColorDark = "#00C2B3";
 
 export const Colors = {
   light: {
