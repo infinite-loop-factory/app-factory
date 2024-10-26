1baf40219e345716e47692631ef22e97d1df9ccd
JAM-PARK <chwk05@gmail.com>
2024-10-26 23:25:08 +0900

    feat: ✨ generate reaction-speed-test

---
+++ apps/reaction-speed-test/hooks/useThemeColor.ts
@@ -0,0 +1,21 @@
+/**
+ * Learn more about light and dark modes:
+ * https://docs.expo.dev/guides/color-schemes/
+ */
+
+import { useColorScheme } from "nativewind";
+
+import { Colors } from "@/constants/Colors";
+
+export function useThemeColor(
+  props: { light?: string; dark?: string },
+  colorName: keyof typeof Colors.light & keyof typeof Colors.dark,
+) {
+  const { colorScheme = "light" } = useColorScheme();
+  const colorFromProps = props[colorScheme];
+
+  if (colorFromProps) {
+    return colorFromProps;
+  }
+  return Colors[colorScheme][colorName];
+}
