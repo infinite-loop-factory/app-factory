870c889e16201a6ac56df948c3bde4f454f21d80
JAM-PARK <chwk05@gmail.com>
2024-10-27 00:08:28 +0900

    chore: 🔧 (tabs) 제거

--- apps/reaction-speed-test/app/_layout.tsx
+++ apps/reaction-speed-test/app/_layout.tsx
@@ -1,11 +1,11 @@
 import "../global.css";
+import HomeScreen from "@/app/index";
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useColorScheme } from "nativewind";
 import { useEffect } from "react";
@@ -34,10 +34,7 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <HomeScreen />
     </ThemeProvider>
   );
 }
