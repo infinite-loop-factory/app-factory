ee4f667f9f2289252814a2896edc74a3616314cf
JAM-PARK <chwk05@gmail.com>
2024-11-10 16:36:11 +0900

    feat: ✨ SmileSvg 추가

--- apps/reaction-speed-test/app/(pages)/measurement.tsx
+++ apps/reaction-speed-test/app/(pages)/measurement.tsx
@@ -1,18 +1,19 @@
-import { Button, ButtonText } from "@/components/ui/button";
-import { Stack, useRouter } from "expo-router";
+import SmileSvg from "@/components/measurement/SmileSvg";
+import SvgWrapper from "@/components/measurement/SvgComponent";
+import { Stack } from "expo-router";
 import type { FC } from "react";
 import { Text, View } from "react-native";
 
 const Measurement: FC = () => {
-  const router = useRouter();
-
   return (
     <View className="flex-1 items-center justify-center">
       <Stack.Screen options={{ title: "측정 페이지", headerShown: false }} />
-      <Text className="mb-5 text-2xl">측정 페이지</Text>
-      <Button className="bg-slate-500" onPress={() => router.back()}>
-        <ButtonText>뒤로 가기</ButtonText>
-      </Button>
+      <Text className="mb-5 text-2xl">
+        이미지가 나타나면 화면을 터치해주세요
+      </Text>
+      <SvgWrapper>
+        <SmileSvg />
+      </SvgWrapper>
     </View>
   );
 };
