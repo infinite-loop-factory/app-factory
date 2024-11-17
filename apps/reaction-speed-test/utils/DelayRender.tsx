870ab8dbb87cb4071d96ae253c731407c9fc03e5
JAM-PARK <chwk05@gmail.com>
2024-11-17 17:42:17 +0900

    feat: ✨ svg 렌더링 전 터치 하는 경우 재시작

--- apps/reaction-speed-test/utils/DelayRender.tsx
+++ apps/reaction-speed-test/utils/DelayRender.tsx
@@ -10,6 +10,7 @@ interface DelayRenderProps {
   minDelay?: number;
   maxDelay?: number;
   onRender?: (delayTime: number) => void;
+  shouldRestart?: boolean;
   fallback?: ReactNode;
 }
 
@@ -18,20 +19,25 @@ export const DelayRender = ({
   minDelay = 1000,
   maxDelay = 3000,
   onRender,
+  shouldRestart = false,
   fallback = null,
 }: PropsWithChildren<DelayRenderProps>) => {
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
-    const delay = random(minDelay, maxDelay);
+    // shouldRestart가 true이거나 초기 렌더링일 때 타이머 시작
+    if (!isVisible || shouldRestart) {
+      const delay = random(minDelay, maxDelay);
+      setIsVisible(false); // 숨김 처리
 
-    const timer = setTimeout(() => {
-      setIsVisible(true);
-      onRender?.(delay);
-    }, delay);
+      const timer = setTimeout(() => {
+        setIsVisible(true);
+        onRender?.(delay);
+      }, delay);
 
-    return () => clearTimeout(timer);
-  }, [minDelay, maxDelay, onRender]);
+      return () => clearTimeout(timer);
+    }
+  }, [minDelay, maxDelay, onRender, shouldRestart, isVisible]);
 
   if (!isVisible) return fallback;
 
