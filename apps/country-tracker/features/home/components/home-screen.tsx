41c6b2b24bf58e3bdbc711764f3c23958080fc96
gracefullight <gracefullight.dev@gmail.com>
2024-12-01 00:48:40 +0900

    feat(country-tracker): ✨ main list

--- apps/country-tracker/features/home/components/home-screen.tsx
+++ apps/country-tracker/features/home/components/home-screen.tsx
@@ -1,59 +1,56 @@
-import { Image, Platform } from "react-native";
-
-import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedView } from "@/components/ThemedView";
+import { Box } from "@/components/ui/box";
+import { Heading } from "@/components/ui/heading";
+import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
 import { Text } from "@/components/ui/text";
-import i18n from "@/i18n";
+import { Search } from "lucide-react-native";
+import type { ListRenderItem } from "react-native";
+import { FlatList } from "react-native";
+
+type CountryItem = {
+  id: string;
+  country: string;
+  flag: string;
+  lastVisitDate: string;
+};
+
+const data: CountryItem[] = [
+  { id: "1", country: "France", flag: "🇫🇷", lastVisitDate: "2023-01-15" },
+  { id: "2", country: "Japan", flag: "🇯🇵", lastVisitDate: "2022-12-05" },
+];
 
 export default function HomeScreen() {
+  const renderItem: ListRenderItem<CountryItem> = ({ item }) => (
+    <Box className="mt-4 flex flex-row justify-between border-gray-100 border-b p-2">
+      <Box className="flex flex-1 flex-row gap-2">
+        <Text>{item.flag}</Text>
+        <Text>{item.country}</Text>
+      </Box>
+      <Text className="font-mono text-gray-500">{item.lastVisitDate}</Text>
+    </Box>
+  );
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          className="absolute bottom-0 left-0 h-[178px] w-[290px]"
-        />
-      }
-    >
-      <ThemedView className="flex-row items-center gap-2">
-        <Text bold size="4xl">
-          {i18n.t("welcome")}!
-        </Text>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView className="mb-2 gap-2">
-        <Text bold size="2xl">
-          Step 1: Try it
-        </Text>
-        <Text>
-          Edit <Text bold>app/(tabs)/index.tsx</Text> to see changes. Press{" "}
-          <Text bold>
-            {Platform.select({ ios: "cmd + d", android: "cmd + m" })}
-          </Text>{" "}
-          to open developer tools.
-        </Text>
-      </ThemedView>
-      <ThemedView className="mb-2 gap-2">
-        <Text bold size="2xl">
-          Step 2: Explore
-        </Text>
-        <Text>
-          Tap the Explore tab to learn more about what's included in this
-          starter app.
-        </Text>
-      </ThemedView>
-      <ThemedView className="mb-2 gap-2">
-        <Text bold size="2xl">
-          Step 3: Get a fresh start
-        </Text>
-        <Text>
-          When you're ready, run <Text bold>npm run reset-project</Text> to get
-          a fresh <Text bold>app</Text> directory. This will move the current{" "}
-          <Text bold>app</Text> to <Text bold>app-example</Text>.
-        </Text>
-      </ThemedView>
+    <ParallaxScrollView>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        ListHeaderComponent={
+          <Box className="flex gap-4">
+            <Heading className="text-2xl text-typography-900">
+              Visited Countries
+            </Heading>
+            <Input className="p-2" variant="outline">
+              <InputField placeholder="Search..." />
+              <InputSlot>
+                <InputIcon as={Search} />
+              </InputSlot>
+            </Input>
+          </Box>
+        }
+        stickyHeaderIndices={[0]}
+      />
     </ParallaxScrollView>
   );
 }
