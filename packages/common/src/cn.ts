2533d2d9179d6fcd428398a4353010874565d874
chakyubeom <ckrbqja@gmail.com>
2024-12-01 09:33:46 +0900

    feat: ✨ cn bug fix

--- packages/common/src/cn.ts
+++ packages/common/src/cn.ts
@@ -6,6 +6,6 @@ import { twMerge } from "tailwind-merge";
  * @param inputs - Class names and conditional classes
  * @returns Combined class name string
  */
-export function cn(...inputs: (string | undefined)[]) {
+export function cn(...inputs: Parameters<typeof clsx>) {
   return twMerge(clsx(inputs));
 }
