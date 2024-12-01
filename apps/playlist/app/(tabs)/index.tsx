8a1e6323d10ee4db452a50d0154df63edfc864e0
gaa0 <lotte1071103@naver.com>
2024-12-01 18:19:26 +0900

    refactor: ♻️ replace StyleSheet with NativeWind className

--- apps/playlist/app/(tabs)/index.tsx
+++ apps/playlist/app/(tabs)/index.tsx
@@ -1,26 +1,9 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello Playlist!</Text>
+    <View className="flex-1 items-center justify-center bg-[#0D0D0D]">
+      <Text className="text-white">Hello Playlist!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#0D0D0D",
-  },
-  text: {
-    color: "white",
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
-  },
-});
