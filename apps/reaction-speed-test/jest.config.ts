4425b1880362b738af65680e4dad58a561ba5983
JAM-PARK <chwk05@gmail.com>
2024-12-01 18:38:17 +0900

    chore: 🔧 rename .js to .ts

--- apps/reaction-speed-test/jest.config.ts
+++ apps/reaction-speed-test/jest.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
   transformIgnorePatterns: [
     "node_modules/(?!(?:.pnpm/)?((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)|@gluestack-ui/.*)",
   ],
-  setupFiles: ["<rootDir>/jest.setup.js"],
+  setupFiles: ["<rootDir>/jest.setup.ts"],
 };
 
 export default config;
