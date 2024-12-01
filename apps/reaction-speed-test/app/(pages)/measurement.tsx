00b8b5b208aac89fafaec7a3a6aab4b310c9019d
JAM-PARK <chwk05@gmail.com>
2024-12-01 18:38:00 +0900

    fix: 🐛 레이아웃 시프팅 방지를 위한 className 수정

--- apps/reaction-speed-test/app/(pages)/measurement.tsx
+++ apps/reaction-speed-test/app/(pages)/measurement.tsx
@@ -7,7 +7,6 @@ import type { FC } from "react";
 import { Pressable, Text, View } from "react-native";
 
 /**
- * TODO: 레이아웃 시프팅 일어나지 않게 자리 고정
  * 다시하기 버튼 홈으로가기 버튼
  */
 const Measurement: FC = () => {
@@ -15,18 +14,24 @@ const Measurement: FC = () => {
 
   return (
     <Pressable onPress={stop} className="flex-1">
-      <View className="flex-1 items-center justify-center">
+      <View className="flex-1 items-center justify-center gap-y-5">
         <Stack.Screen options={{ title: "측정 페이지", headerShown: false }} />
-        <Text className="mb-5 text-2xl">
-          이미지가 나타나면 화면을 터치해주세요
-        </Text>
-        <DelayRender minDelay={1000} maxDelay={3000} onRender={start}>
-          {/* 컴포넌트 이름 파일명 일치하게 수정해야함 */}
-          <SvgWrapper>
-            <SmileSvg />
-          </SvgWrapper>
-        </DelayRender>
-        {result && <Text>반응 시간: {result.reactionTime}ms</Text>}
+        <View className="flex items-center">
+          <Text className="text-2xl">이미지가 나타나면</Text>
+          <Text className="text-2xl">화면을 터치해주세요</Text>
+        </View>
+
+        <View className="h-32 w-32 items-center justify-center">
+          <DelayRender minDelay={1000} maxDelay={3000} onRender={start}>
+            <SvgWrapper>
+              <SmileSvg />
+            </SvgWrapper>
+          </DelayRender>
+        </View>
+
+        <View className="h-8 items-center justify-center">
+          {result && <Text>반응 시간: {result.reactionTime}ms</Text>}
+        </View>
       </View>
     </Pressable>
   );
