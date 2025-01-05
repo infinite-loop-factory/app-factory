f9dd3ca55ee87409d44f14f0d21f6d56b358436c
jihyeonlim <sen03620@naver.com>
2025-01-05 20:51:29 +0900

    feat: ✨ gluestack-ui init

--- apps/dog-walk/babel.config.js
+++ apps/dog-walk/babel.config.js
@@ -10,6 +10,19 @@ module.exports = (api) => {
       ],
       "nativewind/babel",
     ],
-    plugins: ["react-native-reanimated/plugin"],
+    plugins: [
+      "react-native-reanimated/plugin",
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+
+          alias: {
+            "@": "./",
+            "tailwind.config": "./tailwind.config.ts",
+          },
+        },
+      ],
+    ],
   };
 };
