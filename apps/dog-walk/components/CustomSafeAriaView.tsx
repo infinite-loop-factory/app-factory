d870b6a3ff1f506280c1d3560a771f54a952c26c
jihyeonlim <sen03620@naver.com>
2025-01-12 19:34:21 +0900

    fix: 🐛 CustomSafeAriaView 스타일 수정

--- apps/dog-walk/components/CustomSafeAriaView.tsx
+++ apps/dog-walk/components/CustomSafeAriaView.tsx
@@ -4,8 +4,8 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function CustomSafeAreaView({ children }: PropsWithChildren) {
   return (
-    <SafeAreaView className="flex-1 bg-white">
-      <View className="flex-1 px-4">{children}</View>
+    <SafeAreaView className="flex-1 bg-white" edges={["top", "left", "right"]}>
+      <View className="flex-1 bg-white">{children}</View>
     </SafeAreaView>
   );
 }
