44b56d380418fc798fb9af711d763e6c26ddd1c6
jihyeonlim <sen03620@naver.com>
2025-01-12 19:33:34 +0900

    feat: ✨ imagepicker/datepicker 라이브러리 추가

--- apps/dog-walk/app.config.ts
+++ apps/dog-walk/app.config.ts
@@ -30,7 +30,17 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     output: "static",
     favicon: "./assets/images/favicon.png",
   },
-  plugins: ["expo-router", "expo-localization"],
+  plugins: [
+    "expo-router",
+    "expo-localization",
+    [
+      "expo-image-picker",
+      {
+        photosPermission:
+          "The app accesses your photos to let you share them with your friends.",
+      },
+    ],
+  ],
   experiments: {
     tsconfigPaths: true,
     typedRoutes: true,
