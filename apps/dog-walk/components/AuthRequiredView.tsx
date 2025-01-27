25d131b3f5f38fccf74aced2a1b14ce8d027899b
jihyeonlim <sen03620@naver.com>
2025-01-27 14:19:05 +0900

    fix: 🐛 로그인 상태 변경 추가

--- apps/dog-walk/components/AuthRequiredView.tsx
+++ apps/dog-walk/components/AuthRequiredView.tsx
@@ -4,7 +4,17 @@ import { Text } from "@/components/ui/text";
 import { PawPrint } from "lucide-react-native";
 import { View } from "react-native";
 
-export default function AuthRequiredView() {
+interface IAtuthRequiredViewProp {
+  setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function AuthRequiredView({
+  setIsLoggedIn,
+}: IAtuthRequiredViewProp) {
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
     <View className="flex-1 items-center justify-center p-6">
       <View className="w-full max-w-md">
@@ -19,7 +29,11 @@ export default function AuthRequiredView() {
             프로필을 보고 다양한 기능을 이용하려면 로그인해 주세요. 로그인하면
             나만의 산책 코스를 만들고 관리할 수 있어요!
           </Text>
-          <Button className="mb-4 w-full rounded-xl" size={"xl"}>
+          <Button
+            className="mb-4 w-full rounded-xl"
+            size={"xl"}
+            onPress={handleLogin}
+          >
             <ButtonText className="text-white" size="md">
               로그인
             </ButtonText>
