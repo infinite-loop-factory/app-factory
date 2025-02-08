05b7b6f183d3e9b4210a3be225ced37b2d95fd5d
jihyeonlim <sen03620@naver.com>
2025-02-09 00:08:19 +0900

    feat: ✨ 검색 화면>구글맵 추가

--- apps/dog-walk/app/(tabs)/search.tsx
+++ apps/dog-walk/app/(tabs)/search.tsx
@@ -1,9 +1,28 @@
-import { Text, View } from "react-native";
+import { Dimensions, StyleSheet, View } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+
+const { height } = Dimensions.get("window");
 
 export default function SearchScreen() {
   return (
     <View className="flex h-full w-full items-center justify-center">
-      <Text>산책 코스 찾기 화면</Text>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.5665,
+          longitude: 126.978,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        }}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  map: {
+    width: "100%",
+    height: height,
+  },
+});
