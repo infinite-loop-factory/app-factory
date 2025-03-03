3dd7bef6f9cc72e08507bb51a43467bcdd357258
gracefullight <gracefullight.dev@gmail.com>
2025-03-03 12:44:27 +0900

    feat: ✨ webview fab 추가

--- apps/country-tracker/src/components/WebviewLayout.tsx
+++ apps/country-tracker/src/components/WebviewLayout.tsx
@@ -1,3 +1,5 @@
+import { Fab, FabIcon } from "@/components/ui/fab";
+import { Menu, MenuItem } from "@/components/ui/menu";
 import {
   Select,
   SelectBackdrop,
@@ -10,52 +12,49 @@ import {
   SelectPortal,
   SelectTrigger,
 } from "@/components/ui/select";
+import { MenuIcon } from "lucide-react-native";
 import type { ReactNode } from "react";
 import { useEffect, useState } from "react";
 import { Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 type appType = { name: string };
+
 export default function WebviewLayout({ children }: { children: ReactNode }) {
-  const baseURl = "https://infinite-loop-factory.github.io/app-factory/";
+  const baseURL = "https://infinite-loop-factory.github.io/app-factory/";
   const [apps, setApps] = useState<appType[]>([]);
 
   useEffect(() => {
     (async () => {
-      const data: appType[] = await fetch(`${baseURl}app-list.json`).then((d) =>
-        d.json(),
+      const data: appType[] = await fetch(`${baseURL}app-list.json`).then(
+        (res) => res.json(),
       );
       setApps(data);
     })();
   }, []);
 
-  if (typeof window !== "undefined") return children;
+  if (typeof window !== "undefined") return <>{children}</>;
 
   return (
-    <SafeAreaView className={"flex-1 bg-[#b9e2e0]"}>
-      <View className={"flex-1 flex-row items-center justify-center"}>
-        {/* 좌측 메타 정보 */}
-        <View
-          className={
-            "hidden min-w-[500px] flex-[9] flex-row items-center justify-center text-center md:block"
-          }
-        >
+    <SafeAreaView className="flex-1 bg-[#b9e2e0]">
+      <View className="flex-1 flex-row items-center justify-center">
+        {/* 데스크탑용 좌측 메타 정보 */}
+        <View className="hidden min-w-[500px] flex-[9] flex-row items-center justify-center text-center md:block">
           <Image
-            className={"mx-auto"}
+            className="mx-auto"
             style={{ width: 150, height: 150 }}
             source={require("@/assets/images/il.png")}
           />
-
-          <Text className={"block text-center text-[45px]"}>{"무한루프"}</Text>
-          <Text className={"block text-center font-bold text-[45px]"}>
-            {"앱 공장 "}
+          <Text className="block text-center text-[45px]">무한루프</Text>
+          <Text className="block text-center font-bold text-[45px]">
+            앱 공장
           </Text>
           <View className={" flex items-center justify-center p-3"}>
             <Select
               className={"w-[350px] "}
-              selectedValue={location.href.replace(baseURl, "").split("/")[0]}
+              selectedValue={location.pathname.split("/")[0]}
               onValueChange={(d) => {
-                location.href = `${baseURl}${d}`;
+                location.href = `${baseURL}${d}`;
               }}
             >
               <SelectTrigger
@@ -72,7 +71,7 @@ export default function WebviewLayout({ children }: { children: ReactNode }) {
                 <SelectIcon className="mr-3" />
               </SelectTrigger>
               <SelectPortal className={"text-[33px] text-red"}>
-                <SelectBackdrop className={" text-red"} />
+                <SelectBackdrop className="text-red" />
                 <SelectContent className={"p-[100px] text-red"}>
                   <SelectDragIndicatorWrapper>
                     <SelectDragIndicator />
@@ -86,11 +85,8 @@ export default function WebviewLayout({ children }: { children: ReactNode }) {
           </View>
         </View>
 
-        {/* 내용 */}
         <View
-          className={
-            "h-full flex-[4] overflow-hidden md:h-[70%] md:min-w-[400px] md:max-w-[400px] "
-          }
+          className="h-full flex-[4] overflow-hidden md:h-[70%] md:min-w-[400px] md:max-w-[400px]"
           style={{
             shadowColor: "#000",
             shadowOpacity: 0.25,
@@ -98,16 +94,35 @@ export default function WebviewLayout({ children }: { children: ReactNode }) {
             elevation: 8,
           }}
         >
-          <View className={"flex-1"}>
-            <View className={"flex-1"} style={{ backgroundColor: "#FFF" }}>
+          <View className="flex-1">
+            <View className="flex-1" style={{ backgroundColor: "#FFF" }}>
               {children}
             </View>
           </View>
         </View>
 
-        {/*  나머지 영역 */}
-        <View className={"hidden flex-[4] md:block"} />
+        <View className="hidden flex-[4] md:block" />
       </View>
+      <Menu
+        placement="top"
+        offset={5}
+        trigger={({ ...triggerProps }) => (
+          <Fab {...triggerProps}>
+            <FabIcon as={MenuIcon} />
+          </Fab>
+        )}
+      >
+        {apps.map(({ name }) => (
+          <MenuItem
+            key={name}
+            onPress={() => {
+              location.href = `${baseURL}${name}`;
+            }}
+          >
+            <Text>{name}</Text>
+          </MenuItem>
+        ))}
+      </Menu>
     </SafeAreaView>
   );
 }
