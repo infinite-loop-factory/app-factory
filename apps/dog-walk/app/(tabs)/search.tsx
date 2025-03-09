96cf2eb9143c260bb5005322737d31d5ca9eaf2d
jihyeonlim <sen03620@naver.com>
2025-03-09 22:20:03 +0900

    chore: 🔧 검색 화면>더미 데이터 변경

--- apps/dog-walk/app/(tabs)/search.tsx
+++ apps/dog-walk/app/(tabs)/search.tsx
@@ -10,10 +10,7 @@ import { FlatList, GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function SearchScreen() {
   const bottomSheetRef = useRef<BottomSheet>(null);
-  const [location, setLocation] = useState({
-    latitude: 0,
-    longitude: 0,
-  });
+  const [location, setLocation] = useState({ latitude: 0, longitude: 0 });
 
   const snapPoints = useMemo(() => [`${50}%`, `${100}%`], []);
 
@@ -21,39 +18,47 @@ export default function SearchScreen() {
     return [
       {
         id: "1",
-        title: "추천 산책 코스 1",
+        title: "서초구 산책 코스",
         image: "http://via.placeholder.com/280",
         distance: 2.1,
         totalTime: 21,
         address: "몽마르뜨 공원 (서초구)",
         rate: 4.8,
+        latitude: 37.49646,
+        longitude: 127.0041,
       },
       {
         id: "2",
-        title: "추천 산책 코스 2",
+        title: "마포구 산책 코스",
         image: "http://via.placeholder.com/280",
         distance: 2.1,
         totalTime: 21,
         address: "하늘공원 메타세콰이어길 (마포구)",
         rate: 4.6,
+        latitude: 37.56527,
+        longitude: 126.8834,
       },
       {
         id: "3",
-        title: "도시 공원 코스 3",
+        title: "노원구 산책 코스",
         image: "http://via.placeholder.com/280",
         distance: 2.1,
         totalTime: 21,
         address: "경춘선 숲길 (노원구)",
         rate: 4.4,
+        latitude: 37.61977,
+        longitude: 127.0815,
       },
       {
         id: "4",
-        title: "도시 공원 코스 4",
+        title: "강남구 산책 코스",
         image: "http://via.placeholder.com/280",
         distance: 2.1,
         totalTime: 21,
-        address: "강남구 삼성동",
+        address: "삼성 해맞이 공원",
         rate: 4.2,
+        latitude: 37.51976,
+        longitude: 127.062,
       },
     ];
   }, []);
@@ -90,7 +95,11 @@ export default function SearchScreen() {
   return (
     <CustomSafeAreaView>
       <GestureHandlerRootView className="flex-1">
-        <KakaoMap latitude={location.latitude} longitude={location.longitude} />
+        <KakaoMap
+          data={courseList}
+          latitude={location.latitude}
+          longitude={location.longitude}
+        />
         <BottomSheet ref={bottomSheetRef} snapPoints={snapPoints}>
           <BottomSheetView className="flex-1 items-center p-4">
             <FlatList
