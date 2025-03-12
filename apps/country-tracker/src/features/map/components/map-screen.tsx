9fea89f674f3182726e71981f90f4fbff0949308
gracefullight <gracefullight.dev@gmail.com>
2025-03-12 22:10:34 +0900

    feat(country-tracker): ✨ 지구본 추가

--- apps/country-tracker/src/features/map/components/map-screen.tsx
+++ apps/country-tracker/src/features/map/components/map-screen.tsx
@@ -1,22 +1,18 @@
 import { ThemedView } from "@/components/ThemedView";
-import { useAsyncEffect, usePlatform } from "@reactuses/core";
-import { noop } from "es-toolkit";
 import * as Location from "expo-location";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MapView from "react-native-maps";
 
 export default function MapScreen() {
   const [region, setRegion] = useState({
     latitude: 37.7749,
     longitude: -122.4194,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
+    latitudeDelta: 75,
+    longitudeDelta: 75,
   });
 
-  const { platform } = usePlatform();
-
-  useAsyncEffect(
-    async () => {
+  useEffect(() => {
+    (async () => {
       const location = await Location.getCurrentPositionAsync();
       if (location) {
         setRegion((prev) => ({
@@ -25,19 +21,15 @@ export default function MapScreen() {
           longitude: location.coords.longitude,
         }));
       }
-    },
-    noop,
-    [],
-  );
+    })();
+  }, []);
 
   return (
     <ThemedView className="flex-1">
       <MapView
         style={{ flex: 1 }}
         showsCompass={false}
-        // 플랫폼별 mapType 설정
-        // iOS: hybridFlyover, 안드로이드: satellite
-        mapType={platform === "android" ? "satellite" : "hybridFlyover"}
+        mapType="satelliteFlyover"
         region={region}
       />
     </ThemedView>
