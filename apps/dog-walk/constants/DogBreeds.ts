0c02ba8f34361c0dfb00504bef54fe6b64ffb137
jihyeonlim <sen03620@naver.com>
2025-06-01 22:38:45 +0900

    feat: ✨ DogBreeds constant 파일 추가

---
+++ apps/dog-walk/constants/DogBreeds.ts
@@ -0,0 +1,30 @@
+import type { Option } from "@/types/option";
+
+export const dogBreeds: Option[] = [
+  { label: "골든 리트리버", value: "golden-retriever" },
+  { label: "닥스훈트", value: "dachshund" },
+  { label: "도베르만", value: "doberman" },
+  { label: "래브라도 리트리버", value: "labrador-retriever" },
+  { label: "말티즈", value: "maltese" },
+  { label: "보더 콜리", value: "border-collie" },
+  { label: "비숑 프리제", value: "bichon-frise" },
+  { label: "사모예드", value: "samoyed" },
+  { label: "시바견", value: "shiba-inu" },
+  { label: "시츄", value: "shih-tzu" },
+  { label: "스피츠", value: "spitz" },
+  { label: "슈나우저", value: "schnauzer" },
+  { label: "요크셔 테리어", value: "yorkshire-terrier" },
+  { label: "잭 러셀 테리어", value: "jack-russell-terrier" },
+  { label: "치와와", value: "chihuahua" },
+  { label: "코커 스패니얼", value: "cocker-spaniel" },
+  { label: "푸들", value: "poodle" },
+  { label: "포메라니안", value: "pomeranian" },
+  { label: "프렌치 불독", value: "french-bulldog" },
+  { label: "웰시 코기", value: "welsh-corgi" },
+  { label: "기타", value: "etc" },
+];
+
+export const dogGender: Option[] = [
+  { label: "남아", value: "MALE" },
+  { label: "여야", value: "FEMALE" },
+];
