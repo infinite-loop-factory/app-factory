9f4bac8b56bd8e16ca67edabff079bfaafe62a28
jihyeonlim <sen03620@naver.com>
2025-06-03 22:09:52 +0900

    feat: ✨ 반려견 등록 화면 UI 개발

--- apps/dog-walk/app/(screens)/_layout.tsx
+++ apps/dog-walk/app/(screens)/_layout.tsx
@@ -4,6 +4,7 @@ export default function ScreenLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="detail/[id]" options={{ gestureEnabled: false }} />
+      <Stack.Screen name="dog/register" options={{ gestureEnabled: false }} />
     </Stack>
   );
 }
