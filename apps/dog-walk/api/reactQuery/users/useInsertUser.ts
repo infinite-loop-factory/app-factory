fe8a5933411448fa089bac4b73fa9b11e898b7bd
jihyeonlim <sen03620@naver.com>
2025-06-15 20:13:07 +0900

    feat: ✨ Supabase에 사용자 정보를 생성하는 React Query 훅 구현

---
+++ apps/dog-walk/api/reactQuery/users/useInsertUser.ts
@@ -0,0 +1,32 @@
+import { supabase } from "@/api/supabaseClient";
+import { useMutation } from "@tanstack/react-query";
+
+interface InsertUserPayload {
+  id: string;
+  email: string;
+  name: string;
+  profileImageUrl: string;
+}
+
+const insertUser = async ({
+  id,
+  email,
+  name,
+  profileImageUrl,
+}: InsertUserPayload) => {
+  const { error } = await supabase.from("users").insert({
+    id,
+    created_at: new Date(),
+    email,
+    name,
+    profile_image_url: profileImageUrl,
+  });
+
+  if (error) throw error;
+};
+
+export const useInsertUser = () => {
+  return useMutation({
+    mutationFn: (payload: InsertUserPayload) => insertUser(payload),
+  });
+};
