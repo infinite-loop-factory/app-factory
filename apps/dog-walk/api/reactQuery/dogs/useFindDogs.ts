a08497a0a714582de8601076c4c24b6e568d36f7
jihyeonlim <sen03620@naver.com>
2025-06-29 19:47:38 +0900

    feat: ✨ 사용자의 반려견 목록 조회를 위한 React Qeury 훅 추가

---
+++ apps/dog-walk/api/reactQuery/dogs/useFindDogs.ts
@@ -0,0 +1,24 @@
+import { supabase } from "@/api/supabaseClient";
+import { useQuery } from "@tanstack/react-query";
+import { queryKeys } from "../queryKeys";
+
+const fetchDogs = async (userId: string) => {
+  if (!userId) return;
+
+  const { data, error } = await supabase
+    .from("dogs")
+    .select("*")
+    .eq("user_id", userId)
+    .order("created_at", { ascending: true });
+
+  if (error) throw error;
+  return data;
+};
+
+export const useFindDogs = (userId: string) => {
+  return useQuery({
+    queryKey: [queryKeys.dogs.findDogs, userId],
+    queryFn: () => fetchDogs(userId),
+    enabled: !!userId,
+  });
+};
