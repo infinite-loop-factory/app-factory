7a87b2d3ce581efe1b8a1fc6eb5ed097f140b437
jihyeonlim <sen03620@naver.com>
2025-09-07 19:49:02 +0900

    feat: ✨ 검색 화면에 근처 산책 코스 조회 적용 및 목록 표시 UI 컴포넌트 수정

--- apps/dog-walk/components/card/CourseDetailCard.tsx
+++ apps/dog-walk/components/card/CourseDetailCard.tsx
@@ -1,26 +1,26 @@
+import type { CourseRow } from "@/types/course";
+
 import { router } from "expo-router";
 import { MapPinnedIcon, StarIcon } from "lucide-react-native";
 import { Image } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import Images from "@/assets/images";
+import { formatDistanceKm } from "@/utils/number";
 import { HStack } from "../ui/hstack";
 import { Icon } from "../ui/icon";
 import { Text } from "../ui/text";
 import { VStack } from "../ui/vstack";
 
-type TCourseDetailCardProp = {
-  item: {
-    id: string;
-    title: string;
-    image: string;
-    distance: number;
-    totalTime: number;
-    address: string;
-    rate: number;
-  };
-};
-
-export default function CourseDetailCard({ item }: TCourseDetailCardProp) {
-  const { id, title, distance, totalTime, address, rate } = item;
+export default function CourseDetailCard({ item }: { item: CourseRow }) {
+  const {
+    id,
+    start_name,
+    average_rating,
+    total_distance,
+    total_time,
+    end_name,
+    image_url,
+  } = item;
 
   return (
     <TouchableOpacity
@@ -28,34 +28,35 @@ export default function CourseDetailCard({ item }: TCourseDetailCardProp) {
         router.push({ pathname: "/(screens)/detail/[id]", params: { id } });
       }}
     >
-      <HStack className="border-slate-200 border-b p-4">
+      <HStack className="items-center border-slate-200 border-b p-4">
         <Image
           className="mr-4 h-20 w-20 rounded-lg object-cover"
-          source={require("../../assets/images/walking-main-1.png")}
+          source={image_url ? { uri: image_url } : Images.walkingMainImage}
         />
         <VStack className="flex-1 justify-between">
           <HStack className="items-start justify-between">
-            <Text className="flex-1 font-semibold">{title}</Text>
+            <Text className="flex-1 font-semibold">{start_name} 출발</Text>
             <HStack className="items-center">
               <Icon
                 as={StarIcon}
                 className="h-4 w-4 fill-current text-yellow-400"
               />
-              <Text size={"sm"} className="ml-1">
-                {rate}
+              <Text className="ml-1" size={"sm"}>
+                {average_rating}
               </Text>
             </HStack>
           </HStack>
-          <Text size={"sm"} className="text-slate-500">
-            {distance}km • {totalTime}분
+          <Text className="text-slate-500" size={"sm"}>
+            {formatDistanceKm(total_distance)}km • {Math.round(total_time / 60)}
+            분
           </Text>
           <HStack className="items-center overflow-hidden">
             <Icon
               as={MapPinnedIcon}
               className="mr-1 h-4 w-4 text-primary-500"
             />
-            <Text size={"sm"} className="text-ellipsis">
-              {address}
+            <Text className="text-ellipsis" size={"sm"}>
+              {end_name}
             </Text>
           </HStack>
         </VStack>
