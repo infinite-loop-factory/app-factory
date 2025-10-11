b4364eb4c34ff75fc047921d487e8a9f0b0297af
jihyeonlim <sen03620@naver.com>
2025-10-11 20:03:44 +0900

    feat: ✨ 최근 리뷰 조회 훅 적용 및 컴포넌트 데이터 표시 수정

--- apps/dog-walk/components/card/ReviewCard.tsx
+++ apps/dog-walk/components/card/ReviewCard.tsx
@@ -1,17 +1,11 @@
+import type { ReviewDataType } from "@/types/review";
+
 import { Dimensions, Image, Text, View } from "react-native";
+import dayjs from "@/library/dayjs";
+import { maskName } from "@/utils/string";
 
-type TCourseCardProp = {
-  item: {
-    id: string;
-    image: string;
-    name: string;
-    review: string;
-    createdAt: string;
-  };
-};
-
-export default function ReviewCard({ item }: TCourseCardProp) {
-  const { name, review, createdAt } = item;
+export default function ReviewCard({ item }: { item: ReviewDataType }) {
+  const { content, created_at, users } = item;
 
   const screenWidth = Dimensions.get("window").width;
   const calculatedWidth = screenWidth - 110;
@@ -19,18 +13,26 @@ export default function ReviewCard({ item }: TCourseCardProp) {
   return (
     <View className="mb-4 flex w-full flex-row rounded-lg border border-slate-200 p-4">
       <Image
-        source={require("../../assets/images/blank-image.png")}
         className="mr-4 h-10 w-10 rounded-full"
+        source={
+          users.profile_image_url
+            ? { uri: users.profile_image_url }
+            : require("../../assets/images/blank-image.png")
+        }
       />
-      <View style={{ width: calculatedWidth }} className="flex flex-column">
+      <View className="flex flex-column" style={{ width: calculatedWidth }}>
         <View className="flex flex-row justify-between">
-          <Text className="mb-2 font-semibold text-md">{name}</Text>
-          <Text className=" text-slate-500 text-sm">{createdAt}</Text>
+          <Text className="mb-2 font-semibold text-md">
+            {maskName(users.name)}
+          </Text>
+          <Text className=" text-slate-500 text-sm">
+            {dayjs(created_at).format("YYYY-MM-DD HH:mm")}
+          </Text>
         </View>
 
         <View className="flex flex-row overflow-hidden">
           <Text className="mr-2 mb-2 text-slate-500 text-sm" numberOfLines={2}>
-            {review}
+            {content}
           </Text>
         </View>
       </View>
