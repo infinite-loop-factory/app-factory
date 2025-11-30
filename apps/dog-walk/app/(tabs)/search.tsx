69e285207c8e633971e337a4fb0205b1e068a123
jihyeonlim <sen03620@naver.com>
2025-11-30 19:57:07 +0900

    feat: ✨ 검색 화면 차단 반영 및 빈 상태 UI 개선

--- apps/dog-walk/app/(tabs)/search.tsx
+++ apps/dog-walk/app/(tabs)/search.tsx
@@ -1,22 +1,37 @@
 import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 import * as Location from "expo-location";
 import { useFocusEffect } from "expo-router";
+import { useAtomValue } from "jotai";
 import { useCallback, useMemo, useRef, useState } from "react";
 import { Alert, Linking, View } from "react-native";
 import { FlatList, GestureHandlerRootView } from "react-native-gesture-handler";
 import { useFindNearbyCourses } from "@/api/reactQuery/course/useFindNearbyCourses";
+import { userAtom } from "@/atoms/userAtom";
 import CustomSafeAreaView from "@/components/CustomSafeAriaView";
 import CourseDetailCard from "@/components/card/CourseDetailCard";
+import EmptyCourse from "@/components/molecules/EmptyCourse";
 import NaverMap from "@/components/NaverMap";
 import LocationLoading from "@/components/organisms/LocationLoading";
 
 export default function SearchScreen() {
+  const userInfo = useAtomValue(userAtom);
+
   const bottomSheetRef = useRef<BottomSheet>(null);
   const [location, setLocation] = useState({ latitude: 0, longitude: 0 });
 
   const snapPoints = useMemo(() => [`${50}%`, `${100}%`], []);
 
-  const { data: courseList = [] } = useFindNearbyCourses(location);
+  const {
+    data: courseList = [],
+    refetch,
+    isLoading,
+  } = useFindNearbyCourses({
+    ...location,
+    userId: userInfo.id,
+  });
+
+  const hasLocation = location.latitude !== 0 && location.longitude !== 0;
+  const showEmptyState = !isLoading && hasLocation;
 
   useFocusEffect(
     useCallback(() => {
@@ -44,30 +59,32 @@ export default function SearchScreen() {
       }
 
       getCurrentLocation();
-    }, []),
+      refetch();
+    }, [refetch]),
   );
 
   return (
     <CustomSafeAreaView>
       <GestureHandlerRootView className="h-full flex-1">
         <View className="h-1/2 w-full">
-          {location.latitude !== 0 && location.longitude !== 0 && (
+          {hasLocation && (
             <NaverMap
               data={courseList}
               latitude={location.latitude}
               longitude={location.longitude}
             />
           )}
-          {(location.latitude === 0 || location.longitude === 0) && (
-            <LocationLoading />
-          )}
+          {!hasLocation && <LocationLoading />}
         </View>
         <BottomSheet ref={bottomSheetRef} snapPoints={snapPoints}>
-          <BottomSheetView className="flex-1 items-center p-4">
+          <BottomSheetView className="h-full flex-1 items-center p-4">
             <FlatList
               className="w-full"
               data={courseList}
               keyExtractor={(item) => `search_${item.id}`}
+              ListEmptyComponent={
+                showEmptyState ? <EmptyCourse size="md" /> : null
+              }
               renderItem={CourseDetailCard}
             />
           </BottomSheetView>
