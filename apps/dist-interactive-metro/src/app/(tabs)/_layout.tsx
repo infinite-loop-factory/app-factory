b08c5dc4c239e8035ec98f0bc502dbb4ced65464
soki <woosukplay3@gmail.com>
2026-01-18 22:00:21 +0900

    feat(dist-interactive-metro): init new app module
    - pnpm gen create-module
    - path: apps/dist-interactive-metro

---
+++ apps/dist-interactive-metro/src/app/(tabs)/_layout.tsx
@@ -0,0 +1,43 @@
+import { Tabs } from "expo-router";
+import { useColorScheme } from "nativewind";
+import { TabBarIcon } from "@/components/navigation/tab-bar-icon";
+import { COLORS } from "@/constants/colors";
+import i18n from "@/i18n";
+
+export default function TabLayout() {
+  const { colorScheme } = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: COLORS[colorScheme ?? "light"].tint,
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: i18n.t("home"),
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              color={color}
+              name={focused ? "home" : "home-outline"}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              color={color}
+              name={focused ? "code-slash" : "code-slash-outline"}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
