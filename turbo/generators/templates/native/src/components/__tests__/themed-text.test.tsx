ef7aa6e73b0e8818017e668139e55208c86560af
ckrbqja <ckrbqja@gmail.com>
2026-03-01 12:21:51 +0900

    feat: 파일 누락 복구

--- turbo/generators/templates/native/src/components/__tests__/themed-text.test.tsx
+++ turbo/generators/templates/native/src/components/__tests__/themed-text.test.tsx
@@ -1,8 +1,8 @@
 import { render } from "@testing-library/react-native";
-import { ThemedText } from "@/components/ui/themed-text";
+import { ThemedText } from "@/components/themed-text";
 
 it("renders correctly", () => {
-  const tree = render(<ThemedText>Snapshot test!</ThemedText>);
+    const tree = render(<ThemedText>Snapshot test!</ThemedText>);
 
-  expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot();
 });
